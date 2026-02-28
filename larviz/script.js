5ed261a88939d7e46f27309d99bf58f612279470
Meghasri <meghasankar2006@gmail.com>
2026-02-28 19:43:07 +0530

    Update script.js

--- larviz/script.js
+++ larviz/script.js
@@ -1,6 +1,6 @@
 const menu = document.getElementById('mobile-menu');
-const navLinks = document.querySelector('.nav-links');
 
+const navLinks = document.querySelector('.nav-links');
 menu.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
