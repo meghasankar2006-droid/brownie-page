889db91637f6f6a231fa7ea67d38183db80ba3c6
Meghasri <meghasankar2006@gmail.com>
2026-08-04 23:56:57 +0530

    Update script.js

--- larviz/script.js
+++ larviz/script.js
@@ -4,7 +4,6 @@ const navLinks = document.querySelector('.nav-links');
 menu.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
-
 // product card
 document.querySelectorAll('.product-card').forEach(card => {
     const minusBtn = card.querySelector('.minus');
